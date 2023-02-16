78f483caed2636951cf1c5875321e9e9325337e8
Melissa Appadoo <114985119+melissaappadoo@users.noreply.github.com>
2023-02-16 14:50:00 +0000

    initial commit

---
+++ 2DPlatformer-packed/Assets/Scripts/BasicPlatformer/BasicCharacterController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//--------------------------------------------
+/*Basic Character Controller Includes:  
+    - Basic Jumping
+    - Basic grounding with line traces
+    - Basic horizontal movement
+ */
+//--------------------------------------------
+
+public class BasicCharacterController : MonoBehaviour
+{
+    protected bool facingRight = true;
+    protected bool jumped;
+
+    public float speed = 5.0f;
+    public float jumpForce = 1000;
+
+    private float horizInput;
+
+    public Transform groundedCheckStart;
+    public Transform groundedCheckEnd;
+    public bool grounded;
+
+    public Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Linecast to our groundcheck gameobject if we hit a layer called "Level" then we're grounded
+        grounded = Physics2D.Linecast(groundedCheckStart.position, groundedCheckEnd.position, 1 << LayerMask.NameToLayer("Level"));
+        Debug.DrawLine(groundedCheckStart.position, groundedCheckEnd.position, Color.red);
+
+        //Get Player input 
+        horizInput = Input.GetAxis("Horizontal");
+        //Move Character
+        rb.velocity = new Vector2(horizInput * speed * Time.fixedDeltaTime, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && grounded == true)
+        {
+            jumped = true;
+            Debug.Log("Should jump");
+        }
+
+        if (jumped == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            Debug.Log("Jumping!");
+
+            jumped = false;
+        }
+
+        // Detect if character sprite needs flipping
+        if (horizInput > 0 && !facingRight)
+        {
+            FlipSprite();
+        }
+        else if (horizInput < 0 && facingRight)
+        {
+            FlipSprite();
+        }
+    }
+
+    // Flip Character Sprite
+    void FlipSprite()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+}
